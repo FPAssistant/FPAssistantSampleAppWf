4957386b52cfb0032446679ff758184e8ff406fd
Dawson Bunn <dawson.bunn@cadology.com>
2023-11-27 21:04:12 +0000

    2023.11.27 - Complete FAA Destination form

--- FPAssistantSampleAppWf/Forms/Faa/FaaDestinationForm.Designer.cs
+++ FPAssistantSampleAppWf/Forms/Faa/FaaDestinationForm.Designer.cs
@@ -28,18 +28,190 @@
         /// </summary>
         private void InitializeComponent()
         {
+            LabelLatitude = new Label();
+            LabelLongitude = new Label();
+            TextBoxLatitude = new TextBox();
+            TextBoxLongitude = new TextBox();
+            LabelStartCoordinate = new Label();
+            LabelDestinationCoordinate = new Label();
+            TextBoxDestinationLatitude = new TextBox();
+            TextBoxDestinationLongitude = new TextBox();
+            LabelDistance = new Label();
+            TextBoxDistance = new TextBox();
+            LabelBearing = new Label();
+            TextBoxBearing = new TextBox();
+            ButtonClose = new Button();
+            LinkLabelWGS84Destination = new LinkLabel();
             SuspendLayout();
+            // 
+            // LabelLatitude
+            // 
+            LabelLatitude.AutoSize = true;
+            LabelLatitude.Location = new Point(12, 39);
+            LabelLatitude.Name = "LabelLatitude";
+            LabelLatitude.Size = new Size(50, 15);
+            LabelLatitude.TabIndex = 0;
+            LabelLatitude.Text = "Latitude";
+            // 
+            // LabelLongitude
+            // 
+            LabelLongitude.AutoSize = true;
+            LabelLongitude.Location = new Point(12, 68);
+            LabelLongitude.Name = "LabelLongitude";
+            LabelLongitude.Size = new Size(61, 15);
+            LabelLongitude.TabIndex = 1;
+            LabelLongitude.Text = "Longitude";
+            // 
+            // TextBoxLatitude
+            // 
+            TextBoxLatitude.Location = new Point(101, 36);
+            TextBoxLatitude.Name = "TextBoxLatitude";
+            TextBoxLatitude.Size = new Size(100, 23);
+            TextBoxLatitude.TabIndex = 2;
+            TextBoxLatitude.Text = "51.1481018100";
+            TextBoxLatitude.TextChanged += TextBoxLatitude_TextChanged;
+            // 
+            // TextBoxLongitude
+            // 
+            TextBoxLongitude.Location = new Point(101, 65);
+            TextBoxLongitude.Name = "TextBoxLongitude";
+            TextBoxLongitude.Size = new Size(100, 23);
+            TextBoxLongitude.TabIndex = 3;
+            TextBoxLongitude.Text = "-000.1902779940";
+            TextBoxLongitude.TextChanged += TextBoxLongitude_TextChanged;
+            // 
+            // LabelStartCoordinate
+            // 
+            LabelStartCoordinate.AutoSize = true;
+            LabelStartCoordinate.Location = new Point(101, 9);
+            LabelStartCoordinate.Name = "LabelStartCoordinate";
+            LabelStartCoordinate.Size = new Size(93, 15);
+            LabelStartCoordinate.TabIndex = 4;
+            LabelStartCoordinate.Text = "Start Coordinate";
+            // 
+            // LabelDestinationCoordinate
+            // 
+            LabelDestinationCoordinate.AutoSize = true;
+            LabelDestinationCoordinate.Location = new Point(247, 9);
+            LabelDestinationCoordinate.Name = "LabelDestinationCoordinate";
+            LabelDestinationCoordinate.Size = new Size(129, 15);
+            LabelDestinationCoordinate.TabIndex = 5;
+            LabelDestinationCoordinate.Text = "Destination Coordinate";
+            // 
+            // TextBoxDestinationLatitude
+            // 
+            TextBoxDestinationLatitude.Location = new Point(247, 36);
+            TextBoxDestinationLatitude.Name = "TextBoxDestinationLatitude";
+            TextBoxDestinationLatitude.ReadOnly = true;
+            TextBoxDestinationLatitude.Size = new Size(100, 23);
+            TextBoxDestinationLatitude.TabIndex = 6;
+            // 
+            // TextBoxDestinationLongitude
+            // 
+            TextBoxDestinationLongitude.Location = new Point(247, 65);
+            TextBoxDestinationLongitude.Name = "TextBoxDestinationLongitude";
+            TextBoxDestinationLongitude.ReadOnly = true;
+            TextBoxDestinationLongitude.Size = new Size(100, 23);
+            TextBoxDestinationLongitude.TabIndex = 7;
+            // 
+            // LabelDistance
+            // 
+            LabelDistance.AutoSize = true;
+            LabelDistance.Location = new Point(12, 97);
+            LabelDistance.Name = "LabelDistance";
+            LabelDistance.Size = new Size(74, 15);
+            LabelDistance.TabIndex = 8;
+            LabelDistance.Text = "Distance (m)";
+            // 
+            // TextBoxDistance
+            // 
+            TextBoxDistance.Location = new Point(101, 94);
+            TextBoxDistance.Name = "TextBoxDistance";
+            TextBoxDistance.Size = new Size(100, 23);
+            TextBoxDistance.TabIndex = 9;
+            TextBoxDistance.Text = "10000";
+            TextBoxDistance.TextChanged += TextBoxDistance_TextChanged;
+            // 
+            // LabelBearing
+            // 
+            LabelBearing.AutoSize = true;
+            LabelBearing.Location = new Point(12, 129);
+            LabelBearing.Name = "LabelBearing";
+            LabelBearing.Size = new Size(47, 15);
+            LabelBearing.TabIndex = 10;
+            LabelBearing.Text = "Bearing";
+            // 
+            // TextBoxBearing
+            // 
+            TextBoxBearing.Location = new Point(101, 126);
+            TextBoxBearing.Name = "TextBoxBearing";
+            TextBoxBearing.Size = new Size(100, 23);
+            TextBoxBearing.TabIndex = 11;
+            TextBoxBearing.Text = "000";
+            TextBoxBearing.TextChanged += TextBoxBearing_TextChanged;
+            // 
+            // ButtonClose
+            // 
+            ButtonClose.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            ButtonClose.Location = new Point(362, 178);
+            ButtonClose.Name = "ButtonClose";
+            ButtonClose.Size = new Size(75, 23);
+            ButtonClose.TabIndex = 12;
+            ButtonClose.Text = "Close";
+            ButtonClose.UseVisualStyleBackColor = true;
+            ButtonClose.Click += ButtonClose_Click;
+            // 
+            // LinkLabelWGS84Destination
+            // 
+            LinkLabelWGS84Destination.AutoSize = true;
+            LinkLabelWGS84Destination.Location = new Point(12, 182);
+            LinkLabelWGS84Destination.Name = "LinkLabelWGS84Destination";
+            LinkLabelWGS84Destination.Size = new Size(136, 15);
+            LinkLabelWGS84Destination.TabIndex = 13;
+            LinkLabelWGS84Destination.TabStop = true;
+            LinkLabelWGS84Destination.Text = "API: WGS84Destination()";
+            LinkLabelWGS84Destination.LinkClicked += LinkLabelWGS84Destination_LinkClicked;
             // 
             // FaaDestinationForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(449, 213);
+            Controls.Add(LinkLabelWGS84Destination);
+            Controls.Add(ButtonClose);
+            Controls.Add(TextBoxBearing);
+            Controls.Add(LabelBearing);
+            Controls.Add(TextBoxDistance);
+            Controls.Add(LabelDistance);
+            Controls.Add(TextBoxDestinationLongitude);
+            Controls.Add(TextBoxDestinationLatitude);
+            Controls.Add(LabelDestinationCoordinate);
+            Controls.Add(LabelStartCoordinate);
+            Controls.Add(TextBoxLongitude);
+            Controls.Add(TextBoxLatitude);
+            Controls.Add(LabelLongitude);
+            Controls.Add(LabelLatitude);
             Name = "FaaDestinationForm";
             Text = "Destination from Coordinate, Distance and Bearing (FAA)";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label LabelLatitude;
+        private Label LabelLongitude;
+        private TextBox TextBoxLatitude;
+        private TextBox TextBoxLongitude;
+        private Label LabelStartCoordinate;
+        private Label LabelDestinationCoordinate;
+        private TextBox TextBoxDestinationLatitude;
+        private TextBox TextBoxDestinationLongitude;
+        private Label LabelDistance;
+        private TextBox TextBoxDistance;
+        private Label LabelBearing;
+        private TextBox TextBoxBearing;
+        private Button ButtonClose;
+        private LinkLabel LinkLabelWGS84Destination;
     }
 }
--- FPAssistantSampleAppWf/Forms/Faa/FaaDestinationForm.cs
+++ FPAssistantSampleAppWf/Forms/Faa/FaaDestinationForm.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using FpAssistantCore.General;
+using FpAssistantCore.GeneralAviation;
+using FpAssistantCore.Geographical;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +18,87 @@ namespace FPAssistantSampleAppWf.Forms.Faa
         public FaaDestinationForm()
         {
             InitializeComponent();
+            CalculationDestinationCoordinate();
+        }
+
+        private void CalculationDestinationCoordinate()
+        {
+            (bool status, double latitude) = Utilities.ConvertStringToDouble(TextBoxLatitude.Text);
+            if (status == false)
+            {
+                MessageBox.Show("Unable to understand Latitude value in start coordinate!", Program.AppMessageBoxCaption, MessageBoxButtons.OK);
+                return;
+            }
+            (status, double longitude) = Utilities.ConvertStringToDouble(TextBoxLongitude.Text);
+            if (status == false)
+            {
+                MessageBox.Show("Unable to understand Longitude value in start coordinate!", Program.AppMessageBoxCaption, MessageBoxButtons.OK);
+                return;
+            }
+
+            (status, double bearing) = Utilities.ConvertStringToDouble(TextBoxBearing.Text);
+            if (status == false)
+            {
+                MessageBox.Show("Unable to understand bearing value!", Program.AppMessageBoxCaption, MessageBoxButtons.OK);
+                return;
+            }
+            (status, double distance) = Utilities.ConvertStringToDouble(TextBoxDistance.Text);
+            if (status == false)
+            {
+                MessageBox.Show("Unable to understand distance value!", Program.AppMessageBoxCaption, MessageBoxButtons.OK);
+                return;
+            }
+
+            GeoCoordinate start = new(latitude, longitude);
+            GeoCoordinateBasic destination = start.GeoCoordinateBasic.WGS84Destination(new CompassBearing(bearing), new LinearDistance(distance, LinearDistanceUnits.Metres));
+
+            TextBoxDestinationLatitude.Text = destination.Latitude.ToString();
+            TextBoxDestinationLongitude.Text = destination.Longitude.ToString();
+        }
+
+        private void TextBoxLatitude_TextChanged(object sender, EventArgs e)
+        {
+            CalculationDestinationCoordinate();
+        }
+
+        private void TextBoxLongitude_TextChanged(object sender, EventArgs e)
+        {
+            CalculationDestinationCoordinate();
+        }
+
+        private void TextBoxDistance_TextChanged(object sender, EventArgs e)
+        {
+            CalculationDestinationCoordinate();
+        }
+
+        private void TextBoxBearing_TextChanged(object sender, EventArgs e)
+        {
+            (bool status, double bearing) = Utilities.ConvertStringToDouble(TextBoxBearing.Text);
+
+            if (status)
+            {
+                CompassBearing compassBearing = new(bearing);
+                TextBoxBearing.Text = compassBearing.AsDegrees().ToString();
+                CalculationDestinationCoordinate();
+            }
+        }
+
+        private void ButtonClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void LinkLabelWGS84Destination_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            try
+            {
+                Program.VisitLink(@"http://api.fpassistant.com/api/FpAssistantCore.Geographical.GeoCoordinateBasic.html#FpAssistantCore_Geographical_GeoCoordinateBasic_WGS84Destination_FpAssistantCore_General_CompassBearing_FpAssistantCore_General_LinearDistance_");
+                LinkLabelWGS84Destination.LinkVisited = true;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(Program.AppCantOpenUrlLink, Program.AppMessageBoxCaption);
+            }
         }
     }
 }
